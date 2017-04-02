e5820965e3f423b2bbe9c21c271a3b67cd32566c
Emily Eisenberg <xymostech@gmail.com>
2017-04-02 13:51:58 -0700

    Add executable-y thing

---
+++ src/exe.js
@@ -0,0 +1,21 @@
+// The executable tex parser
+import {setSource} from "./lexer.js";
+import parseHorizontalList, {HBoxChar} from "./parser/horizontalList.js";
+
+process.stdin.setEncoding('utf8');
+
+let source = "";
+process.stdin.on('data', (chunk) => {
+    source += chunk;
+});
+process.stdin.on('end', () => {
+    setSource(source.split('\n'));
+    const result = parseHorizontalList();
+
+    result.forEach(r => {
+        if (r instanceof HBoxChar) {
+            process.stdout.write(r.ch);
+        }
+    });
+    process.stdout.write('\n');
+});
